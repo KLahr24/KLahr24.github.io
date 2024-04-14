21710d44e000cedb619ae5cc67ace84e11c19d5d
KLahr24 <157331254+KLahr24@users.noreply.github.com>
2024-04-13 23:53:20 -0400

    Update server.js
    update

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ app.post('/submit-form', (req, res) => {
 });
 
 // Start the server
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
